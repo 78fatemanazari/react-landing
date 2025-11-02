ffb3bbaeea4034f5a79a089a472a787d5d89b6e9
Fatema Nazari <fatemanazari78@gmail.com>
2025-11-01 21:04:29 -0700

    create abour sections

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { About } from "../about/About"
 import { HeroContainer } from "../herosection/HeroContainer"
 import './Home.css'
 
@@ -5,6 +6,7 @@ export const Home =() =>{
     return(
         <>
             <HeroContainer />
+            <About />
         </>
     )
 }
