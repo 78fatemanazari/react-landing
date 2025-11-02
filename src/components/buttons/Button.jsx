47d06befba033b750abc0b3df00e429207b96b13
Fatema Nazari <fatemanazari78@gmail.com>
2025-11-01 19:14:32 -0700

    change all to persion

---
+++ src/components/buttons/Button.jsx
@@ -0,0 +1,11 @@
+import './Button.css'
+
+export const Button = () =>{
+    return(
+        <>
+            <div className="button">
+                <button>قیمت رایگان</button>
+            </div>
+        </>
+    )
+}
